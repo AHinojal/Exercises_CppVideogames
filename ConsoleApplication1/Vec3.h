7184115af3456b1d6b61ceac690ad8c578db16f1
Álvaro Hinojal <AHinojal@users.noreply.github.com>
2020-10-22 22:16:49 +0200

    delete cpp

--- ConsoleApplication1/Vec3.h
+++ ConsoleApplication1/Vec3.h
@@ -1,30 +1,32 @@
-#pragma once
+#ifndef VECTOR_3
+#define VECTOR_3
+
+template <typename E>
 class Vec3
 {
-	private:
-		int cordX = 0;
-		int cordY = 0;
-		int cordZ = 0;
+
 	public:
 		// Constructor by default
-		Vec3();
-		// Destructor
-		~Vec3();
+		Vec3() : x(0), y(0), z(0) {}
+		// Destructor -> NO NECESSARY
+		// ~Vec3() {}
 
 		// Constructor w/ three values
-		Vec3(int x, int y, int z);
+		Vec3(E x, E y, E z) {}
 		// Copy's Contructor
-		Vec3(const Vec3 &vector);
+		Vec3(const Vec3& vector) {}
 
 		// METHODS
-		int normalize();
-		int distance_to(const Vec3& vector);
-		int dot_product(const Vec3& vector);
-		int cross_product(const Vec3& vector);
-		float angle_between(const Vec3& vector);
+		int normalize() {}
+		int distance_to(const Vec3& vector) {}
+		int dot_product(const Vec3& vector) {}
+		int cross_product(const Vec3& vector) {}
+		float angle_between(const Vec3& vector) {}
 
-
-
-
+	private:
+		E x;
+		E y;
+		E z;
 };
 
+#endif // !VECTOR_3
