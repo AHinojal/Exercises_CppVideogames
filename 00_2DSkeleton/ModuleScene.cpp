4fdab9f65ba7e0fce073bbb47694dd62c42f5855
Álvaro Hinojal <AHinojal@users.noreply.github.com>
2020-10-29 15:36:57 +0100

    Update TODO7 - Skeleton

--- 00_2DSkeleton/ModuleScene.cpp
+++ 00_2DSkeleton/ModuleScene.cpp
@@ -1,6 +1,9 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleScene.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "SDL/include/SDL.h"
 
 ModuleScene::ModuleScene()
 {
@@ -12,11 +15,20 @@ ModuleScene::~ModuleScene()
 
 bool ModuleScene::Init()
 {
+	// Load the selected texture 
+	if (!texture) {
+		texture = App->textures->Load("sprites.png");
+	}
+
 	return true;
 }
 
 update_status ModuleScene::Update()
 {
+	// Show the exactly pixels that we select
+	SDL_Rect space { 50,50,50,50 };
+	App->renderer->Blit(texture,50,50, &space);
+
 	return UPDATE_CONTINUE;
 }
 
--- 00_2DSkeleton/ModuleScene.h
+++ 00_2DSkeleton/ModuleScene.h
@@ -2,15 +2,22 @@
 
 #include "Module.h"
 #include "Globals.h"
+#include "SDL/include/SDL.h"
+
+struct SDL_Texture;
+struct SDL_Rect;
 
 class ModuleScene : public Module
 {
-public:
-	ModuleScene();
-	~ModuleScene();
+	public:
+		ModuleScene();
+		~ModuleScene();
 
-	bool Init();
-	update_status Update();
-	bool CleanUp();
+		bool Init();
+		update_status Update();
+		bool CleanUp();
+
+	public:
+		SDL_Texture* texture = NULL;
 };
 
